8cfc0f140b1e471f05d75ec365f4bf72463c5051
yurakl <yuraklisko@gmail.com>
2023-08-21 15:35:06 +0300

    paral_1: add parallel search

--- find.c
+++ find.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #define FNLEN 512
+
 // Структура для представлення елемента списку
 struct file_entry {
     char name[FNLEN]; 
@@ -11,9 +15,40 @@ struct file_entry {
     struct file_entry *next;
 };
 
-// Функція для виводу вмісту папки за допомогою звичайного списку
-void list_folder_contents(const char *path, const char *filename) 
+//Multithreading variables
+int max_threads = 0;
+int n_threads = 0;
+
+struct thread_args {
+    char *arg1;
+    char *arg2;
+};
+
+pthread_mutex_t mutex;
+
+struct file_entry * pop_first(struct file_entry * head)
+{  
+	if (head->next) { 
+		head = head->next;  
+		free(head->prev);
+		head->prev = NULL;		
+	} else {
+		free(head);  
+		head = NULL;
+	}  
+	return head;
+}
+
+void *list_folder_contents(void *arg)
 {
+
+	struct thread_args *args = (struct thread_args *) arg;
+	char *path = args->arg1;
+	char *filename = args->arg2;
+
+	pthread_t thread_id = pthread_self();
+	printf("Enter Thread ID: %lu\t%s\n", thread_id, path);
+	
 	DIR *dir;
 	struct dirent *entry;
 	
@@ -21,7 +56,7 @@ void list_folder_contents(const char *path, const char *filename)
 	dir = opendir(path);
 	if (!dir) {
 		perror("opendir");
-		return;
+		return NULL;
 	}
 	
 	struct file_entry *head = (struct file_entry *) malloc(sizeof(struct file_entry));
@@ -31,6 +66,8 @@ void list_folder_contents(const char *path, const char *filename)
 	if (head->name[strlen(head->name - 1)] != '/') {
 		strcat(head->name, "/");		
 	}
+	head->prev = NULL;
+	head->next = NULL;
 	
 	tail = head;  
 	closedir(dir);
@@ -38,11 +75,13 @@ void list_folder_contents(const char *path, const char *filename)
 	
 	while (head)
 	{  
+		sleep(1);
 		dir = opendir(head->name);
 		if (dir) {
 			memset(parent, 0, FNLEN);
 			strcpy(parent, head->name);  
 			while ((entry = readdir(dir)) != NULL) {
+				
 				if (entry->d_type == DT_REG) {
 					
 					if (strcmp(entry->d_name, filename) == 0) {
@@ -62,35 +101,56 @@ void list_folder_contents(const char *path, const char *filename)
 					memset(tail->name, 0, FNLEN);
 					strcpy(tail->name, parent); 
 					strcat(tail->name, entry->d_name); 
-					strcat(tail->name,"/"); 
+					strcat(tail->name,"/");
 				}
 				
 			} 
 			closedir(dir); 
-			
-			if (head->next) {
-				head = head->next; 
-				free(head->prev);
-				
-			}
-			else {
-				free(head); 
-				head = NULL;
-			} 
 		}
-	}
+
+		head = pop_first(head); 
+		
+		if (n_threads < max_threads && head	) {
+			struct thread_args *args = (struct thread_args *) malloc(sizeof(struct thread_args));
+			args->arg1 = (char *) malloc(strlen(head->name) + 1);
+			strcpy(args->arg1, head->name);
+			args->arg2 = (char *) malloc(strlen(filename) + 1);
+			strcpy(args->arg2, filename);
+			pthread_t thread;
+			pthread_create(&thread, NULL, list_folder_contents, args);
+			head = pop_first(head);
+			pthread_mutex_lock(&mutex);
+			n_threads++;
+			pthread_mutex_unlock(&mutex);			
+
+		}
+		
+		
+	} 
+	pthread_mutex_lock(&mutex);
+	n_threads--;
+	pthread_mutex_unlock(&mutex);
+	return NULL;
 }
 
 int main(int argc, char *argv[]) 
 {
+	clock_t start_time = clock();
 	if (argc < 3) {
 		printf("Usage: %s folder_path filename\n", argv[0]);
 		return 1;
 	}
 
-	const char *folder_path = argv[1];
-	const char *filename = argv[2];
-	list_folder_contents(folder_path, filename);
+	struct thread_args arg = {argv[1], argv[2]};
+
+	pthread_mutex_init(&mutex, NULL);
+
+	list_folder_contents(&arg);
+	clock_t end_time = clock();
+
+	double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    	printf("Execution time: %f seconds\n", execution_time);
 
 	return 0;
 }
